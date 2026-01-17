603bfcf9241d4646430474ec88a5697e049b0bb5
Francisco Jamo Junior <franciscojamojunior@gmail.com>
2026-01-17 16:59:53 +0200

    2nd push

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,6 @@
 // import { Plus } from "lucide-react";
 import { useState } from "react";
+import Input from "./input";
 
 function AddTask({ AdicionarTarefa }) {
   const [title, setTitle] = useState("");
@@ -7,20 +8,20 @@ function AddTask({ AdicionarTarefa }) {
 
   return (
     <div className="bg-slate-200 p-6 rounded-md space-y-4 shadow flex flex-col">
-      <input
+      <Input
         type="text"
         id="in"
         value={title}
         placeholder="digite o titulo da tarefa "
-        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        // className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
         onChange={(event) => setTitle(event.target.value)}
       />
-      <input
+      <Input
         type="text"
         id="in"
         value={description}
         placeholder="descricao da tarefa"
-        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        // className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
         onChange={(event) => setDescription(event.target.value)}
       />
       <button
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,12 @@
 function Input(props) {
   return (
     <input
-      type={props.type}
-      value={props.value}
-      placeholder={props.placeholder}
+      //   type={props.type}
+      //   value={props.value}
+      //   placeholder={props.placeholder}
+      //   onChange={props.onChange}
+      {...props}
       className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
-      onChange={props.onChange}
     />
   );
 }
